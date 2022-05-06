a25d8f44bc28d0963baf86ce5242d7571f1a2a47
lucassss-li <lucas8qwer@gmail.com>
2022-05-06 14:17:49 +0800

    MyParameters

---
+++ src/function/MyParameters.ts
@@ -0,0 +1,20 @@
+import { Expect } from '../utils/Expect'
+import { Equal } from '../utils/Equal'
+
+namespace MyParameters {
+    type MyParameters<T extends (...args: any[]) => any> = T extends (
+        ...args: infer R
+    ) => any
+        ? R
+        : never
+
+    const foo = (arg1: string, arg2: number): void => {}
+    const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
+    const baz = (): void => {}
+
+    type cases = [
+        Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+        Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+        Expect<Equal<MyParameters<typeof baz>, []>>
+    ]
+}
