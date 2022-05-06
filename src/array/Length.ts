73d4ebb2f5f8f848b4c2d1b9a9d7d125ea699428
lucassss-li <lucas8qwer@gmail.com>
2022-05-06 13:55:50 +0800

    Length

---
+++ src/array/Length.ts
@@ -0,0 +1,22 @@
+namespace Length {
+    type Length<T> = T extends any[]
+        ? T['length']
+        : T extends readonly any[]
+        ? T['length']
+        : never
+
+    const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+    const spaceX = [
+        'FALCON 9',
+        'FALCON HEAVY',
+        'DRAGON',
+        'STARSHIP',
+        'HUMAN SPACEFLIGHT'
+    ] as const
+    type cases = [
+        Expect<Equal<Length<typeof tesla>, 4>>,
+        Expect<Equal<Length<typeof spaceX>, 5>>,
+        Length<5>,
+        Length<'hello world'>
+    ]
+}
