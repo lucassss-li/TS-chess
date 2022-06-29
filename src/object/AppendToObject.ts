a79d970e4cee17fafa93b514f6e4ce11dc3f0eaf
lucassss-li <lucas8qwer@gmail.com>
2022-06-29 11:02:50 +0800

    AppendToObject

---
+++ src/object/AppendToObject.ts
@@ -0,0 +1,55 @@
+import { Expect } from '../utils/Expect'
+import { Equal } from '../utils/Equal'
+namespace AppendToObject {
+    type AppendToObject<T, U extends string, V> = {
+        [K in U | keyof T]: K extends keyof T ? T[K] : V
+    }
+
+    type test1 = {
+        key: 'cat'
+        value: 'green'
+    }
+
+    type testExpect1 = {
+        key: 'cat'
+        value: 'green'
+        home: boolean
+    }
+
+    type test2 = {
+        key: 'dog' | undefined
+        value: 'white'
+        sun: true
+    }
+
+    type testExpect2 = {
+        key: 'dog' | undefined
+        value: 'white'
+        sun: true
+        home: 1
+    }
+
+    type test3 = {
+        key: 'cow'
+        value: 'yellow'
+        sun: false
+    }
+
+    type testExpect3 = {
+        key: 'cow'
+        value: 'yellow'
+        sun: false
+        isMotherRussia: false | undefined
+    }
+
+    type cases = [
+        Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
+        Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
+        Expect<
+            Equal<
+                AppendToObject<test3, 'isMotherRussia', false | undefined>,
+                testExpect3
+            >
+        >
+    ]
+}
