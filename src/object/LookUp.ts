9e335948edbfba177e51d90c4aede93091efbb17
lucassss-li <lucas8qwer@gmail.com>
2022-06-29 10:07:13 +0800

    LookUp

---
+++ src/object/LookUp.ts
@@ -0,0 +1,24 @@
+import { Equal } from '../utils/Equal'
+import { Expect } from '../utils/Expect'
+
+namespace LookUp {
+    type LookUp<U, T> = U extends { type: T } ? U : never
+
+    interface Cat {
+        type: 'cat'
+        breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+    }
+
+    interface Dog {
+        type: 'dog'
+        breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+        color: 'brown' | 'white' | 'black'
+    }
+
+    type Animal = Cat | Dog
+
+    type cases = [
+        Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+        Expect<Equal<LookUp<Animal, 'cat'>, Cat>>
+    ]
+}
