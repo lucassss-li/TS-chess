e69ef2a54676c7cb8d13aae361abdf8d2a89d495
lucassss-li <lucas8qwer@gmail.com>
2022-06-29 11:40:48 +0800

    IsNever

---
+++ src/common/IsNever.ts
@@ -0,0 +1,15 @@
+import { Expect } from '../utils/Expect'
+import { Equal } from '../utils/Equal'
+namespace IsNever {
+    type IsNever<T> = [T] extends [never] ? true : false
+
+    type cases = [
+        Expect<Equal<IsNever<never>, true>>,
+        Expect<Equal<IsNever<never | string>, false>>,
+        Expect<Equal<IsNever<''>, false>>,
+        Expect<Equal<IsNever<undefined>, false>>,
+        Expect<Equal<IsNever<null>, false>>,
+        Expect<Equal<IsNever<[]>, false>>,
+        Expect<Equal<IsNever<{}>, false>>
+    ]
+}
