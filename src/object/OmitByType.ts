db2318a2a7f076f112ce2627746d192573eb5f35
lucassss-li <lucas8qwer@gmail.com>
2022-06-29 17:06:17 +0800

    OmitByType

---
+++ src/object/OmitByType.ts
@@ -0,0 +1,32 @@
+import { Expect } from '../utils/Expect'
+import { Equal } from '../utils/Equal'
+namespace OmitByType {
+    type OmitByType<T, U> = {
+        [K in keyof T as T[K] extends U ? never : K]: T[K]
+    }
+
+    interface Model {
+        name: string
+        count: number
+        isReadonly: boolean
+        isEnable: boolean
+    }
+
+    type cases = [
+        Expect<
+            Equal<OmitByType<Model, boolean>, { name: string; count: number }>
+        >,
+        Expect<
+            Equal<
+                OmitByType<Model, string>,
+                { count: number; isReadonly: boolean; isEnable: boolean }
+            >
+        >,
+        Expect<
+            Equal<
+                OmitByType<Model, number>,
+                { name: string; isReadonly: boolean; isEnable: boolean }
+            >
+        >
+    ]
+}
