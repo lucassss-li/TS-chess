6deca7c22a84b38d848fa0e613dd65db4e9a9602
lucassss-li <lucas8qwer@gmail.com>
2022-06-29 09:51:36 +0800

    First

--- src/array/First.ts
+++ src/array/First.ts
@@ -4,6 +4,7 @@ namespace First {
     type First<T extends any[]> = T extends [infer first, ...infer Rest]
         ? first
         : never
+    type First2<T extends any[]> = T['length'] extends 0 ? never : T[0]
     type cases = [
         Expect<Equal<First<[3, 2, 1]>, 3>>,
         Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
