6ae6bd30bab4f7b0323d8479cc4c69a1eb0d3709
lucassss-li <lucas8qwer@gmail.com>
2022-07-04 15:15:03 +0800

    Trunc

---
+++ src/number/Trunc.ts
@@ -0,0 +1,18 @@
+import { Expect } from '../utils/Expect'
+import { Equal } from '../utils/Equal'
+namespace Trunc {
+    type Trunc<
+        T extends number | string,
+        S extends string = `${T}`
+    > = S extends `${infer L}.${infer R}` ? L : S
+
+    type cases = [
+        Expect<Equal<Trunc<0.1>, '0'>>,
+        Expect<Equal<Trunc<1.234>, '1'>>,
+        Expect<Equal<Trunc<12.345>, '12'>>,
+        Expect<Equal<Trunc<-5.1>, '-5'>>,
+        Expect<Equal<Trunc<'1.234'>, '1'>>,
+        Expect<Equal<Trunc<'-10.234'>, '-10'>>,
+        Expect<Equal<Trunc<10>, '10'>>
+    ]
+}
